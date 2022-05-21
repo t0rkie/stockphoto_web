f39e0eb387dd30481a5b52520cbb624aa89801fe
t0rkie <surfin8000@gmail.com>
2022-05-22 08:21:46 +0900

    ファイルリサイズ

---
+++ pages/test.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react"
+import Resizer from "react-image-file-resizer";
+import Image from "next/image"
+
+const InputImage = () => {
+  const [image, setImage] = useState<string | null>(null)
+
+  const resizeImage = (file: Blob): Promise<string> => (
+    new Promise(resolve => (
+      Resizer.imageFileResizer(
+        file,
+        300,
+        400,
+        'JPEG',
+        100,
+        0,
+        (uri) => {
+          resolve(uri as string)
+        },
+        'base64'
+      )
+    ))
+  )
+
+  const onChange = async(event: any) => {
+    try {
+      const file = event.target.files[0]
+      const image = await resizeImage(file)
+      setImage(image)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
+    <>
+      <input type="file" onChange={onChange} />
+      {image && <Image src={image} width={300} height={300} alt="test" />}
+    </>
+  )
+}
+
+export default InputImage
